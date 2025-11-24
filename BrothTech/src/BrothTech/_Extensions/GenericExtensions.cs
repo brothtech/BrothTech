271336f228ca865752ca75355bc293e5455a42cf
ben <ben@brothtech.com>
2025-11-23 22:27:40 -0800

    extensions

---
+++ BrothTech/src/BrothTech/_Extensions/GenericExtensions.cs
@@ -0,0 +1,16 @@
+﻿#pragma warning disable IDE0130 // Namespace does not match folder structure
+using System.Runtime.CompilerServices;
+
+namespace BrothTech;
+
+public static class GenericExtensions
+{
+    public static T EnsureNotNull<T>(
+        this T? value,
+        [CallerArgumentExpression(nameof(value))]
+        string? name = null)
+        where T : class
+    {
+        return value ?? throw new ArgumentNullException(name ?? nameof(value));
+    }
+}
---
+++ BrothTech/src/BrothTech/_Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿#pragma warning disable IDE0130 // Namespace does not match folder structure
+using System.Diagnostics.CodeAnalysis;
+
+namespace BrothTech;
+
+public static class StringExtensions
+{
+    public static bool IsNullOrWhiteSpace(
+        [NotNullWhen(false)]
+        this string? value)
+    {
+        return string.IsNullOrWhiteSpace(value);
+    }
+
+    public static bool IsNullOrEmpty(
+        [NotNullWhen(false)]
+        this string? value)
+    {
+        return string.IsNullOrEmpty(value); 
+    }
+}
